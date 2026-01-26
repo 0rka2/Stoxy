8253e620890577a20fd58a0fd2e9acb33054bc00
0rka2 <132233779+0rka2@users.noreply.github.com>
2026-01-26 14:09:24 +1300

    Fix deploy error

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,12 +1,13 @@
 import {serve} from "inngest/next";
 import {inngest} from "@/lib/inngest/client";
 import {
-    sendDailyNewsSummary,
+    // sendDailyNewsSummary,
     sendNewsUser,
     sendSignUpEmail
 } from "@/lib/inngest/functions";
 
 export const { GET, POST, PUT } = serve({
     client: inngest,
-    functions: [sendSignUpEmail,sendDailyNewsSummary,sendNewsUser],
+    functions: [sendSignUpEmail,sendNewsUser],
+    // functions: [sendSignUpEmail,sendDailyNewsSummary,sendNewsUser],
 });
